9a63ea44fbab6d79074a6293263a55dc466f21dc
archslaveCW <archslave@naver.com>
2018-06-28 10:48:37 +0900

    2018. 6.28. 1050i - archslaveCW
    - Just simple examples of google maps api

---
+++ navi/NavigationCW/app/src/main/java/ssu/cse/navigationcw/MapsActivity.java
@@ -0,0 +1,49 @@
+package ssu.cse.navigationcw;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(37.525007, 126.971547);
+        LatLng sydney2 = new LatLng(37.625007, 127.071547);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("마크1"));
+        mMap.addMarker(new MarkerOptions().position(sydney2).title("마크2"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
