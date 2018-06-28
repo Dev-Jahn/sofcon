6f1fefabe15f0c4b0556762303e07c25837aa10a
archslaveCW <archslave@naver.com>
2018-06-28 22:14:43 +0900

    2018. 6.28. 2215i archslaveCW
    @ Permission request code is added / modified
      - I need display UI

--- navi/NavigationCW/app/src/main/java/ssu/cse/navigationcw/MainActivity.java
+++ navi/NavigationCW/app/src/main/java/ssu/cse/navigationcw/MainActivity.java
@@ -5,35 +5,64 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     // Request code for ACCESS_FINE_LOCATION
     private static final int REQUEST_CODE_LOCATION = 3125;
+    private Button buttonMap;
+    private Button buttonPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonMap = (Button)findViewById(R.id.buttonMap);
+        buttonPlace = (Button)findViewById(R.id.buttonPlace);
+
         /**
          * Check Permissions
          * - ACCESS_FINE_LOCATION
          *
-         * @ IF permission is not allowed,
+         * @ IF permission was not allowed,
          *   We should show some dialogs that why we needs this permissions
          *   @ NOT IMPLEMENTED YET!!!!!
          *   @ by archslaveCW
          */
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // Request missing location permission.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_CODE_LOCATION);
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Display UI and wait for user interaction
+
+
+            } else {
+                // Request missing location permission.
+                ActivityCompat.requestPermissions(
+                        this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_LOCATION);
+            }
         } else {
             // Location permission has been granted, continue as usual.
 
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == REQUEST_CODE_LOCATION) {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // IF permission was allowed
+
+            } else {
+                // IF Permission was denied or request was cancelled
+            }
         }
     }
 }
