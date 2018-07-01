c6d07dea405a741cc5821fc5125c78b5088b891d
mixify <mixify@naver.com>
2018-07-01 16:39:54 +0900

    login activity => get id, passwd

--- ui/app/src/main/java/com/example/tjsan/ui/MainActivity.java
+++ ui/app/src/main/java/com/example/tjsan/ui/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.tjsan.ui;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = (Button) findViewById(R.id.login_btn);
+
+        btn_login.setOnClickListener(LoginClickListener);
     }
+    Button.OnClickListener LoginClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view) {
+            String id;
+            String pw;
+            id = ((TextInputEditText) findViewById(R.id.id_text)).getText().toString();//use this as a view
+            pw = ((TextInputEditText) findViewById(R.id.pw_text)).getText().toString();
+        }
+    };
 }
