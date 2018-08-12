92045c39e781f3b4a7fa25986134ebff57aea967
Dev-Jahn <ajh508@naver.com>
2018-08-12 20:21:59 +0900

    redesigned scripts

---
+++ recomm/corpus_maker_tag.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import re
+
+df = pd.read_csv('data/hotel_review_pp.csv')
+pd.DataFrame.head(df)
+pd.DataFrame.tail(df)
+
+#리뷰단위 분할
+text = ''
+text = text.join([review+'\n' for review in df['review']])
+text = re.sub(r'[^ 가-힣0-9\n]',' ',text)
+text = re.sub(r' +',' ',text)
+array = text.split('\n')[:]
+
+#형태소 분리
+from konlpy.tag import Mecab
+mecab = Mecab()
+tagged = [mecab.pos(sentence) for sentence in array]
+
+#corpus 저장
+import pickle
+corpus = [mecab.morphs(sentence) for sentence in array]
+with open('corpus/tag-based_hotel.list','wb') as f:
+    pickle.dump(corpus, f)
+
+#태그로 사용할 형태소 분류만 추출
+pattern = re.compile('MM|NNG|VA[+].*|VV[+].*|XR')
+df_tags = pd.DataFrame(columns = ['score','tags'], dtype = 'int64')
+taglist = []
+for place in tagged:
+    tag = np.array(place)
+    npbool = []
+    for t in tag:
+        npbool.append(re.fullmatch(pattern,t[1])!=None)
+    tag = tag[npbool].tolist()
+    taglist.append(tag)
+df_tags['tags'] = taglist
+df_tags['score'] = df['score'].astype('int64')
+print(df_tags['tags'][0])
