24700464216ae9892ba21c7cf50228766ab9cbca
mixify <mixify@naver.com>
2018-08-21 13:04:59 +0900

    changing Main Activity

--- ui/app/src/main/java/com/example/kyeon/myapplication/MainActivity.java
+++ ui/app/src/main/java/com/example/kyeon/myapplication/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.kyeon.myapplication;
 
 import android.drm.DrmStore;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;;
 import android.content.Intent;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.Space;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.outline_list_black_18dp);
         actionBar.setDisplayHomeAsUpEnabled(true);
-
 
         Button bNewTrip = (Button)findViewById(R.id.newTrip);
         bNewTrip.setOnClickListener(new View.OnClickListener() {
