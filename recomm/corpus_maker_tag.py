b33a5f433db395f5810a8b50fe6b64b953044918
Dev-Jahn <ajh508@naver.com>
2018-08-23 12:56:51 +0900

    test created corpus for attraction_kor, elapsed time:40mins

--- recomm/corpus_maker_tag.py
+++ recomm/corpus_maker_tag.py
@@ -1,65 +1,112 @@
+
+# coding: utf-8
+
+# # # 1안
+# # 영문: 토큰화, 모든 단어를 기본형으로 변환, 유의미 품사 추출
+# # 한글: 형태소 분석, 유의미 형태소 추출
+# # 각 단어별로 장소 임베딩
+# # # 2안
+# # word2vec으로 모든 단어를 임베딩한 후,
+# # k-means clustering으로 군집화하여 군집별로 장소 임베딩
+
+# In[106]:
+
+
+import numpy as np
 import pandas as pd
 import re
-
-####호텔####
-df = pd.read_csv('data/kor/hotel_review_pp.csv')
-#리뷰단위 분할
-text = ''
-text = text.join([review+'\n' for review in df['review']])
-text = re.sub(r'[^ 가-힣0-9\n]',' ',text)
-text = re.sub(r' +',' ',text)
-array = text.split('\n')[:]
-#형태소 분리
-from konlpy.tag import Mecab
-mecab = Mecab()
-tagged = [mecab.pos(sentence) for sentence in array]
-#corpus 저장
+import os
+import time
 import pickle
-corpus = [mecab.morphs(sentence) for sentence in array]
-with open('corpus/tag-based_hotel.list','wb') as f:
-    pickle.dump(corpus, f)
-
-####명소####
-df = pd.read_csv('data/kor/attraction_review_pp.csv')
-text = ''
-text = text.join([review+'\n' for review in df['review']])
-text = re.sub(r'[^ 가-힣0-9\n]',' ',text)
-text = re.sub(r' +',' ',text)
-array = text.split('\n')[:]
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import konlpy
 from konlpy.tag import Mecab
-mecab = Mecab()
-tagged = [mecab.pos(sentence) for sentence in array]
-import pickle
-corpus = [mecab.morphs(sentence) for sentence in array]
-with open('corpus/tag-based_attraction.list','wb') as f:
-    pickle.dump(corpus, f)
 
-####식당####
-df = pd.read_csv('data/kor/restaurant_review_pp.csv')
-text = ''
-text = text.join([review+'\n' for review in df['review']])
-text = re.sub(r'[^ 가-힣0-9\n]',' ',text)
-text = re.sub(r' +',' ',text)
-array = text.split('\n')[:]
-from konlpy.tag import Mecab
-mecab = Mecab()
-tagged = [mecab.pos(sentence) for sentence in array]
-import pickle
-corpus = [mecab.morphs(sentence) for sentence in array]
-with open('corpus/tag-based_restaurant.list','wb') as f:
-    pickle.dump(corpus, f)
+nltk.data.path.append('F:\\소공전프로젝트\\sofcon\\recomm\\nltk_data')
 
-#태그로 사용할 형태소 분류만 추출
-'''pattern = re.compile('MM|NNG|VA[+].*|VV[+].*|XR')
-df_tags = pd.DataFrame(columns = ['score','tags'], dtype = 'int64')
+list_csv = ['data/kor/attraction_review_tag.csv',
+            'data/kor/hotel_review_tag.csv',
+            'data/kor/restaurant_review_tag.csv',
+            'data/eng/eng_attraction_review_tag.csv',
+            'data/eng/eng_hotel_review_tag.csv',
+            'data/eng/eng_restaurant_review_tag.csv']
+list_corpus = ['corpus/attraction_tag.list',
+               'corpus/hotel_tag.list',
+               'corpus/restaurant_tag.list']
+try:
+    os.stat('corpus')
+except:
+    os.mkdir('corpus')
+
+
+# In[79]:
+
+
+def orderset(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+# # 한글
+
+# In[40]:
+
+
+df = pd.read_csv(list_csv[0])
+# filter charset exception
+df['review'] = df['review'].apply(lambda x: re.sub(r'[^ 가-힣0-9.!?\n]',' ',x))
+# make sentence list
+array = df['review'].tolist()
+# 한글형태소 분리
+mecab = Mecab()
+list_pos = [mecab.pos(sentence) for sentence in array]
+# 형태소 리스트화
+morpheme = [mecab.morphs(sentence) for sentence in array]
+
+
+# In[57]:
+
+
+# 의미를 가지는 형태소만 추출
+pattern = re.compile('MM|NNG|VA[+].*|VV[+].*|XR')
+df_morpheme = pd.DataFrame(columns = ['placeId','tags'], dtype = 'int64')
 taglist = []
-for place in tagged:
-    tag = np.array(place)
+for i in range(len(list_pos)):
+    pairs = np.array(list_pos[i])
+    tags = np.array(morpheme[i])
     npbool = []
-    for t in tag:
-        npbool.append(re.fullmatch(pattern,t[1])!=None)
-    tag = tag[npbool].tolist()
-    taglist.append(tag)
-df_tags['tags'] = taglist
-df_tags['score'] = df['score'].astype('int64')
-print(df_tags['tags'][0])'''
+    for pair in pairs:
+        npbool.append(re.fullmatch(pattern,pair[1])!=None)
+    tags = tags[npbool]
+    taglist.append(tags.tolist())
+df_morpheme['tags'] = taglist
+df_morpheme['placeId'] = df['placeId'].astype('int64')
+
+
+# In[94]:
+
+
+wordlist = []
+for l in df_morpheme['tags']:
+    wordlist += l
+wordset = orderset(wordlist)
+print('전체', len(wordlist))
+print('집합', len(wordset))
+
+
+# In[104]:
+
+
+start = time.time()
+corpus = []
+for word in wordset:
+    places = []
+    for i in range(len(df_morpheme)):
+        if word in df_morpheme['tags'][i]:
+           places.append(df_morpheme['placeId'][i])
+    corpus.append(places)
+print(corpus[1])
+print('Elapsed time: ', str(time.time() - start))
