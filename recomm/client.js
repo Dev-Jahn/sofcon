5c9527f48c85e2e18e92d82f6b355147228d31b9
Dev-Jahn <ajh508@naver.com>
2018-09-07 21:49:48 +0900

    added server prototype

---
+++ recomm/client.js
@@ -0,0 +1,13 @@
+var zerorpc = require("zerorpc");
+
+var client = new zerorpc.Client();
+client.connect("tcp://127.0.0.1:4242");
+console.log("start");
+
+client.invoke("recommend",
+		'attraction',
+		['324890','3808315','590748'],
+		['320359'],
+		function(error, res, more) {
+			console.log(res);
+		});
