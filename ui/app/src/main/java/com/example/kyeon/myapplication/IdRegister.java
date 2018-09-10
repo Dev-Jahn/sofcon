688c5d55702a7dec007c961540e80074804ae2f9
Park DaeKyeong <kyeongparks@gmail.com>
2018-09-10 12:32:34 +0900

    password view change

--- ui/app/src/main/java/com/example/kyeon/myapplication/IdRegister.java
+++ ui/app/src/main/java/com/example/kyeon/myapplication/IdRegister.java
@@ -57,19 +57,19 @@ public class IdRegister extends Activity {
                 name_owner = register_name_txt.getText().toString();
                 phoneNO = register_phoneNO_txt.getText().toString();
 
-                Log.d("DEBUG-TEST", id + id.length());
-                Log.d("DEBUG-TEST", passwd + passwd.length());
-                Log.d("DEBUG-TEST", passwdCM + passwdCM.length());
-                Log.d("DEBUG-TEST", name_owner + name_owner.length());
-                Log.d("DEBUG-TEST", phoneNO + phoneNO.length());
+                //Log.d("DEBUG-TEST", id + id.length());
+                //Log.d("DEBUG-TEST", passwd + passwd.length());
+                //Log.d("DEBUG-TEST", passwdCM + passwdCM.length());
+                //Log.d("DEBUG-TEST", name_owner + name_owner.length());
+                //Log.d("DEBUG-TEST", phoneNO + phoneNO.length());
 
                 //---------------------id input logic------------------------//
                 if(id.length() == 0) {
-                    Log.d("ERROR-msg", "no id input");
+                    //Log.d("ERROR-msg", "no id input");
                     id_input_error.setText("* id가 입력되지 않았습니다.       ");
                 }
                 else if(id.length() < 6) {
-                    Log.d("ERROR-msg", "short id input");
+                    //Log.d("ERROR-msg", "short id input");
                     id_input_error.setText("* id는 6자리 이상의 영문입니다.    ");
                 }
                 else if(id.length() > 6) {
@@ -77,7 +77,7 @@ public class IdRegister extends Activity {
                         if(id.charAt(i) >= 'a' && id.charAt(i) <= 'z') continue;
                         else if(id.charAt(i) >= 'A' && id.charAt(i) <= 'Z') continue;
                         else {
-                            Log.d("ERROR-msg", "id type not identified");
+                            //Log.d("ERROR-msg", "id type not identified");
                             id_input_error.setText("* id는 6자리 이상의 영문입니다.        ");
                             break;
                         }
@@ -92,11 +92,11 @@ public class IdRegister extends Activity {
 
                 //-----------------------passwd input logic----------------------------//
                 if(passwd.length() == 0) {
-                    Log.d("ERROR-msg", "no input passwd");
+                    //Log.d("ERROR-msg", "no input passwd");
                     passwd_input_error.setText("* 패스워드가 입력되지 않았습니다.     ");
                 }
                 else if(passwd.length() < 6) {
-                    Log.d("ERROR-msg", "short passwd");
+                    //Log.d("ERROR-msg", "short passwd");
                     passwd_input_error.setText("* 패스워드는 6자리 이상의 영문/기호/숫자의 조합입니다.    ");
                 }
                 else passwd_i = 1;
@@ -104,12 +104,12 @@ public class IdRegister extends Activity {
 
                 //--------------------------passswd commit logic-------------------------//
                 if(passwd.length() != passwdCM.length()) {
-                    Log.d("ERROR-msg", "passwd commit not matching");
+                    //Log.d("ERROR-msg", "passwd commit not matching");
                     passwd_commit_error.setText("* 패스워드와 일치하지 않습니다.     ");
                 }
 
                 else if(!passwd.equals(passwdCM)) {
-                    Log.d("ERROR-msg", "passwd not matching");
+                    //Log.d("ERROR-msg", "passwd not matching");
                     passwd_commit_error.setText("* 패스워드와 일치하지 않습니다.     ");
                 }
 
@@ -118,28 +118,28 @@ public class IdRegister extends Activity {
 
                 //--------------------------name input logic -----------------------------//
                 if(name_owner.length() == 0) {
-                    Log.d("ERROR-msg", "no input name");
+                    //Log.d("ERROR-msg", "no input name");
                     name_input_error.setText("* 이름이 입력되지 않았습니다.     ");
                 }
                 else name_i = 1;
                 ////////////////////////////////////////////////////////////////////////////
 
                 if(phoneNO.length() == 0) {
-                    Log.d("ERROR-msg", "no input phone no");
+                    //Log.d("ERROR-msg", "no input phone no");
                     phoneNO_input_error.setText("* 전화번호가 입력되지 않았습니다.    ");
                 }
                 else if(phoneNO.length() != 11) {
-                    Log.d("ERROR-msg", "phone NO type incorrect");
+                    //Log.d("ERROR-msg", "phone NO type incorrect");
                     phoneNO_input_error.setText("* 전화번호는 11자리의 숫자입니다.   ");
                 }
                 else if(phoneNO.contains("-")) {
-                    Log.d("ERROR-msg", "remove '-' symbol");
+                    //Log.d("ERROR-msg", "remove '-' symbol");
                     phoneNO_input_error.setText("'-' 기호를 생략하여 입력 해주세요.  ");
                 }
                 else if(phoneNO.length() == 11) {
                     for(int i = 0; i < phoneNO.length(); i++) {
                         if(phoneNO.charAt(i) < '0' || phoneNO.charAt(i) > '9') {
-                            Log.d("ERROR-msg", "number should be in");
+                            //Log.d("ERROR-msg", "number should be in");
                             phoneNO_input_error.setText("* 올바르지 않은 번호입니다.   ");
                             break;
                         }
@@ -148,9 +148,9 @@ public class IdRegister extends Activity {
                 }
                 else phone_i = 1;
 
-                Log.d("DATA-OUTPUT", id_i+" "+passwd_i+" "+passwdCM_i+" "+name_i+" "+phone_i+" "+"hello");
+                //Log.d("DATA-OUTPUT", id_i+" "+passwd_i+" "+passwdCM_i+" "+name_i+" "+phone_i+" "+"hello");
                 if(id_i == 1 && passwd_i == 1 && passwdCM_i == 1 && name_i == 1 && phone_i == 1) {
-                    Log.d("DATA-OUTPUT", "commited");
+                    //Log.d("DATA-OUTPUT", "commited");
                     finish();
                 }
             }
